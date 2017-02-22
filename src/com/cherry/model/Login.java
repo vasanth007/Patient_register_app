1e34026f70bac4e476769b7fbed709f9872b3e36
vasanth007 <vsnthd68@gmail.com>
2017-02-22 18:25:27 +0530

    register works,login 500

--- src/com/cherry/model/Login.java
+++ src/com/cherry/model/Login.java
@@ -14,30 +14,18 @@ import com.cherry.pojo.Regist;
 public class Login 
 {
 
-	public static Boolean GetLogin(String name, String pass) 
+	public static Boolean GetLogin(String username, String password) 
 	{
-//		String n = ""; 
-//		String p = "";
-		 String searchName = name;
 		Session session=Config.Hiber();
 		Transaction transaction = session.beginTransaction();
-		Query query = session.createQuery("FROM Regist where pname=:n and password=:p");
-		 query.setParameter("n",name);
-		 query.setParameter("p",pass);
-         List list = query.list();
-         System.out.println(list);
-         
-Iterator iterator = list.iterator();
-         
-         while (iterator.hasNext()) {//is next data exist? then loop
-             Regist obj = (Regist) iterator.next();//cast and assign next data to Person type object
-             
-             int id =obj.getId();
-             System.out.println("inside iterator "+id);
-         }
-         transaction.commit();
-         session.close();
-         
+		
+	        Query query = session.createQuery("from Register where username=:u and password=:p");
+	        query.setParameter("u",username);
+	        query.setParameter("p",password);
+	       List list=query.list(); 
+	        System.out.println(list);
+	        transaction.commit();
+			session.close();
          if(list.toString()=="[]")
          {
         	 return true;
@@ -46,18 +34,6 @@ Iterator iterator = list.iterator();
          {
         	 return false;
          }
-//         String searchId = "1";
-//         String query = "from Register as r where r.id = :sId";
-//         List list = session.createQuery(query)
-//                     .setString("sId", searchId).list();
-//         
-         
-		
-		//session.save(object)
-	
-	
-	
-		//Close.Close1(transaction,reg,session);
 			
 	}
 }
